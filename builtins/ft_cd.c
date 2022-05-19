0b368a23b5e7ec38cc890260cee95e64154cf280
hermin9804 <hermin9804@gmail.com>
2022-05-20 02:02:45 +0900

    feat: builtin cd 구현

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,59 +6,43 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 18:48:13 by mher              #+#    #+#             */
-/*   Updated: 2022/05/19 20:29:25 by mher             ###   ########.fr       */
+/*   Updated: 2022/05/20 01:53:22 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h> //chdir()
-#include <stdio.h> //printf
+#include <unistd.h> //chdir
 #include <stdlib.h> //free
-#include "libft/include/libft.h"
+#include "libft/include/libft.h" 
 
-int	ft_pwd(void) //test
+static int	cd_home(const char *path)
 {
-	char	*buf;
+	char	*home;
 
-	buf = getcwd(NULL, 0);
-	if (buf == 0)
+	home = getenv("HOME");
+	if (path == 0)
+		return (chdir(home));
+	else if (ft_strlen(path) == 1 && ft_strncmp("~", path, 1) == 0)
+		return (chdir(home));
+	else if (ft_strncmp("~/", path, 2) == 0)
 	{
-		//print_error();
-		return (-1);
+		home = ft_strjoin(home, path + 1);
+		if (home == NULL)
+		{
+			//print_error(); ??
+			return (-1);
+		}
+		chdir(home);
+		free(home);
 	}
-	printf("%s\n", buf);
-	free(buf);
 	return (0);
 }
 
-
-void	cd_path(const char *path)
-{
-	chdir(path);
-}
-
-void	cd_home(char *envp[])
-{
-	while (*envp && ft_strncmp("HOME=", *envp, 5))
-		++envp;
-	//if (envp == NULL)
-	//	error();
-	cd_path(*envp + 5);	
-}
-
-void	ft_cd(const char *path, char *envp[])
+int	ft_cd(const char *path)
 {
 	if (path == 0)
-		cd_home(envp);
-	else if (ft_strlen(path) == 1 && ft_strncmp("~", path, 1) == 0)
-		cd_home(envp);
+		return (cd_home(path));
+	else if (ft_strncmp("~", path, 1) == 0)
+		return (cd_home(path));
 	else
-		cd_path(path);
-}
-
-int main(int argc, char *argv[], char *envp[]) //test
-{
-	ft_pwd();
-	ft_cd("~", envp);
-	ft_pwd();
-	return (0);
+		return (chdir(path));
 }
