7c678d0de80c9c132d1c3582d2833c54ac22de66
hermin9804 <hermin9804@gmail.com>
2022-05-20 20:34:14 +0900

    ft_echo 구현

--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -6,28 +6,63 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 18:36:23 by mher              #+#    #+#             */
-/*   Updated: 2022/05/20 19:31:24 by mher             ###   ########.fr       */
+/*   Updated: 2022/05/20 20:33:19 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./builtin.h"
 
-int	ft_echo(int argc, char *args[], char *envp[]) 
+static int	check_option_n(int argc, char *argv[], int *idx)
+{
+	int i;
+	int n_option;
+
+	i = 1;
+	n_option = 0;
+	if (argc < 2)
+		return (n_option);
+	while (i < argc && ft_strncmp(argv[i], "-n", 2) == 0)
+	{
+		n_option = 1;
+		++i;
+	}
+	*idx = i;
+	return (n_option);
+}
+
+static void	echo_env_value(const char *key, char *envp[])
+{
+	char	*value;
+	(void)envp;
+
+//todo: exit_status를 전역변수로 받으면 이런식으로 처리 가능
+//	if (key[1] == '?')
+//		print_exit_status();
+//	else
+//	{
+		value = getenv(key); //todo: minishell 자체 ft_getenv() 로 바꿔야함
+		write(STDOUT_FILENO, value, ft_strlen(value));
+//	}
+//
+//	else
+//	{
+//		value = ft_getenv(key, envp);
+//	}
+}
+
+int	ft_echo(int argc, char *argv[], char *envp[])
 {
 	int	i;
 	int	n_option;
 
-	(void)envp;
 	i = 1;
-	n_option = 0;
-	if (ft_strncmp(args[i], "-n", 2) == 0)
-	{
-		n_option = 1;
-		i = 2;
-	}
+	n_option = check_option_n(argc, argv, &i);
 	while (i < argc)
 	{
-		write(STDOUT_FILENO, args[i], ft_strlen(args[i]));
+		if (argv[i][0] == '$')
+			echo_env_value(argv[i], envp);
+		else
+			write(STDOUT_FILENO, argv[i], ft_strlen(argv[i]));
 		if (i + 1 != argc)
 			write(STDOUT_FILENO, " ", 1);
 		++i;
