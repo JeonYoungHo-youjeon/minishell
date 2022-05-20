9a4309f779e4263c174b70e0868a5a719d4dc7ef
hermin9804 <hermin9804@gmail.com>
2022-05-20 18:14:05 +0900

    refactor: return 방법 개선

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,35 +6,34 @@
 /*   By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 18:48:13 by mher              #+#    #+#             */
-/*   Updated: 2022/05/20 01:53:22 by mher             ###   ########.fr       */
+/*   Updated: 2022/05/20 18:12:25 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h> //chdir
-#include <stdlib.h> //free
-#include "libft/include/libft.h" 
+#include "./builtin.h"
 
 static int	cd_home(const char *path)
 {
 	char	*home;
+	int	ret;
 
 	home = getenv("HOME");
 	if (path == 0)
-		return (chdir(home));
+		ret = chdir(home);
 	else if (ft_strlen(path) == 1 && ft_strncmp("~", path, 1) == 0)
-		return (chdir(home));
+		ret = chdir(home);
 	else if (ft_strncmp("~/", path, 2) == 0)
 	{
 		home = ft_strjoin(home, path + 1);
 		if (home == NULL)
 		{
-			//print_error(); ??
+			//perror(); ??
 			return (-1);
 		}
-		chdir(home);
+		ret = chdir(home);
 		free(home);
 	}
-	return (0);
+	return (ret);
 }
 
 int	ft_cd(const char *path)
