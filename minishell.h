cf94af3bba4acccd0beeb0805c4d91a31e3619fb
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2022-05-24 00:27:08 +0900

    시그널 탈취 및 프롬프트 시작 부분 구현

---
+++ minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youjeon <youjeon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 17:31:34 by youjeon           #+#    #+#             */
+/*   Updated: 2022/05/24 00:26:21 by youjeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <term.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+
+#endif
