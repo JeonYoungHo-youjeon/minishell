7b36032790eaa01ae1acff800c038e61599ce66e
hermin9804 <hermin9804@gmail.com>
2022-06-04 01:19:23 +0900

    FEAT: g_exit_code 설정

--- executor/executor.c
+++ executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: youjeon <youjeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 16:46:29 by mher              #+#    #+#             */
-/*   Updated: 2022/06/02 20:09:39 by mher             ###   ########.fr       */
+/*   Updated: 2022/06/04 01:17:46 by mher             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ static int	is_need_fork(char *cmd)
 		return (0);
 	else if (ft_strcmp(cmd, "unset") == 0)
 		return (0);
+	else if (ft_strcmp(cmd, "exit") == 0)
+		return (0);
 	return (1);
 }
 
-//리눅스 자체 builtin 명령어 수행
+//OS 자체 builtin 명령어 수행
 static int	origin_cmd(t_cmd *cmd, char *envp[])
 {
 	int	i;
@@ -34,13 +36,16 @@ static int	origin_cmd(t_cmd *cmd, char *envp[])
 	ret = 0; //
 	path = ft_split(getenv("PATH"), ':');
 //	if (path);
-//		return ()
+//		return (
 	cmd_path = get_cmd_path(cmd->argv[0], path);
-	//if (arg->cmd_path == NULL)
-		//exit_with_perror("command not found", 127);
+	if (cmd_path == NULL)
+	{
+		perror("command not found");
+		return (127);
+	}
 	ret = execve(cmd_path, cmd->argv, envp);
-	//if (ret == -1)
-		//exit_with_perror("execve fail", EXIT_FAILURE);
+	if (ret == -1)
+		perror("execve fail");
 	i = 0;
 	while(path[i])
 		free(path[i++]);
@@ -49,53 +54,38 @@ static int	origin_cmd(t_cmd *cmd, char *envp[])
 	return (ret);
 }
 
-//fork() 가 필요한 명령어 실행
-static int	execute_do_fork_cmd(t_cmd *cmd, t_env *env_head, char *envp[])
+static int	execute_cmd(t_cmd *cmd, t_env *env_head, char *envp[])
 {
-	int	ret;
+	int	exit_code;
 
-	if (ft_strcmp(cmd->argv[0], "cd") == 0)
-		return (ft_cd(cmd->argv[1]));
-	else if (ft_strcmp(cmd->argv[0], "export") == 0)
-		return (ft_export(cmd->argc, cmd->argv, env_head));
-	else if (ft_strcmp(cmd->argv[0], "unset") == 0)
-		return (ft_unset(cmd->argc, cmd->argv, env_head));
-	else if (ft_strcmp(cmd->argv[0], "pwd") == 0)
-		ret = ft_pwd();
+	if (ft_strcmp(cmd->argv[0], "pwd") == 0)
+		exit_code = ft_pwd();
+	else if (ft_strcmp(cmd->argv[0], "cd") == 0)
+		exit_code = (ft_cd(cmd->argv[1]));
 	else if (ft_strcmp(cmd->argv[0], "env") == 0)
-		ret = ft_env(env_head);
+		exit_code = ft_env(env_head);
 	else if (ft_strcmp(cmd->argv[0], "echo") == 0)
-		ret = ft_echo(cmd->argc, cmd->argv);
+		exit_code = ft_echo(cmd->argc, cmd->argv);
 	else if (ft_strcmp(cmd->argv[0], "exit") == 0)
-		ret = ft_exit(cmd->argc, cmd->argv, env_head);
-	else
-		ret = origin_cmd(cmd, envp);
-	//if (ret == -1)
-	//	perror();
-	exit(EXIT_SUCCESS);
-}
-
-//fork() 가 필요없는 명령어 실행
-static int	execute_not_fork_cmd(t_cmd *cmd, t_env *env_head)
-{
-	if (ft_strcmp(cmd->argv[0], "cd") == 0)
-		return (ft_cd(cmd->argv[1]));
+		exit_code = (ft_exit(cmd, env_head));
 	else if (ft_strcmp(cmd->argv[0], "export") == 0)
-		return (ft_export(cmd->argc, cmd->argv, env_head));
+		exit_code = (ft_export(cmd->argc, cmd->argv, env_head));
 	else if (ft_strcmp(cmd->argv[0], "unset") == 0)
-		return (ft_unset(cmd->argc, cmd->argv, env_head));
+		exit_code = (ft_unset(cmd->argc, cmd->argv, env_head));
 	else
-		return (-1);
+		exit_code = origin_cmd(cmd, envp);
+	return (exit_code);
 }
 
 int	executor(t_cmd *cmd, t_env *env_head, char *envp[])
 {
 	pid_t	pid;
+	int	status;
 	
 	while (cmd != 0)
 	{
-		if (is_need_fork(cmd->argv[0]) == 0)
-			execute_not_fork_cmd(cmd, env_head);
+		if (cmd->prev == NULL && is_need_fork(cmd->argv[0]) == 0)
+			execute_cmd(cmd, env_head, envp);
 		else
 		{
 			if (pipe(cmd->fd) == -1)
@@ -106,15 +96,17 @@ int	executor(t_cmd *cmd, t_env *env_head, char *envp[])
 		 	{
 				redirect(cmd);
 				close_unused_fd(cmd, pid);
-				execute_do_fork_cmd(cmd, env_head, envp);
+				exit(execute_cmd(cmd, env_head, envp));
 			}
 			else
 				close_unused_fd(cmd, pid);
 		}
 		cmd = cmd->next;
 	}
-	while (wait(0) != -1)
-		;
+	while (wait(&status) != -1)
+		g_exit_code = (((status) & 0xff00) >> 8);
+		//(((status) & 0xff00) >> 8) 대신 WIFEXITED(status) 로 쓰면
+		//매크로 define 이라 쓰면 안되나???
 	delete_tmp_file();
 	return (0);
 }
